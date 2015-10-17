e413801e1e53266eee7b2a18bfa2735c14220445
Nicholas Marus <nmarus@gmail.com>
2015-10-17 01:14:07 -0400

    Initial commit

---
+++ build.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+#used to pbuild images for mediadrop
+
+set -e
+
+#colors
+NONE=$(echo -e '\033[00m')
+RED=$(echo -e '\033[00;31m')
+GREEN=$(echo -e '\033[00;32m')
+YELLOW=$(echo -e '\033[00;33m')
+PURPLE=$(echo -e '\033[00;35m')
+CYAN=$(echo -e '\033[00;36m')
+WHITE=$(echo -e '\033[00;37m')
+BOLD=$(echo -e '\033[1m')
+UNDERLINE=$(echo -e '\033[4m')
+
+#print timestamp
+timestamp() {
+  date +"%Y-%m-%d %T"
+}
+
+#screen/file logger
+#$1 = string
+sflog() {
+    #if $1 is not null
+    if [ ! -z ${1+x} ]; then
+        message=$1
+        echo "${CYAN}$(timestamp)${NONE} ${message}"
+    else
+        #exit function
+        return 1;
+    fi
+    #if $LOG_FILE is not null
+    if [ ! -z ${LOG_FILE+x} ]; then
+    #if $2 is regular file or does not exist
+        if [ -f ${LOG_FILE} ] || [ ! -e ${LOG_FILE} ]; then
+            echo "${CYAN}$(timestamp)${NONE} ${message}" >> ${LOG_FILE}
+        fi
+    fi
+}
+
+#screen/file logger
+#read from stdin
+#$1 message
+#$2 color code
+sfpipe() {
+    while read in; do
+        sflog "${2}${1}${NONE} $in"
+    done
+}
+
+#build nginx
+build_nginx() {
+    {
+        #remove image
+        (docker rmi mediadrop-nginx || true &> /dev/null)
+        #pull latest base image
+        docker pull nginx
+        (cd nginx && docker build --rm --no-cache -t mediadrop-nginx . )
+    } | sfpipe nginix ${RED}
+}
+
+#build uwsgi
+build_uwsgi() {
+    {
+        #remove image
+        (docker rmi mediadrop-uwsgi || true &> /dev/null)
+        #pull latest base image
+        docker pull debian:jessie
+        (cd uwsgi && docker build --rm --no-cache -t mediadrop-uwsgi .)
+    } | sfpipe uwsgi ${GREEN}
+}
+
+#build mariadb
+build_mariadb() {
+    {
+        #pull latest base image
+        docker pull mariadb
+    } | sfpipe mariadb ${YELLOW}
+}
+
+#simple args parse
+if [ ! -z ${1+x} ]; then
+    if [ "${1}" = "all" ]; then
+        build_nginx &
+        build_uwsgi &
+        build_mariadb &
+        while (($(ps aux | pgrep docker | wc -l) != 0)); do
+            sleep 5 &> /dev/null
+        done
+        (docker rmi $(docker images -q -f dangling=true) &> /dev/null)
+        exit 0
+    elif [ "${1}" = "nginx" ]; then
+        build_nginx
+        (docker rmi $(docker images -q -f dangling=true) &> /dev/null)
+        exit 0
+    elif [ "${1}" = "uwsgi" ]; then
+        build_uwsgi
+        (docker rmi $(docker images -q -f dangling=true) &> /dev/null)
+        exit 0
+    fi
+else
+    echo "Usage: ${0} all|nginx|uwsgi"
+fi
---
+++ uwsgi/start.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -e
+
+init_mediadrop() {
+
+    #clear directory contents
+    rm -rf /wsgi/*
+    rm -rf /mediadrop/*
+
+    #download mediadrop latest from git
+    git clone https://github.com/mediadrop/mediadrop.git /mediadrop
+
+    #activate python virtual environment
+    source /venv/mediadrop/bin/activate
+
+    #install mediadrop
+    cd /mediadrop
+    pip install aniso8601
+    python /mediadrop/setup.py develop
+
+    #setup mediadrop
+    cd /wsgi
+    paster make-config MediaDrop deployment.ini
+    sed -i 's,email_to = you@yourdomain.com,email_to = '${SMTP_FROM}'@'${SMTP_DOMAIN}',' deployment.ini
+    sed -i 's,smtp_server = localhost,smtp_server = '${SMTP_SERVER}',' deployment.ini
+    sed -i 's,error_email_from = paste@localhost,error_email_from = '${SMTP_FROM}'@'${SMTP_DOMAIN}',' deployment.ini
+    sed -i 's,mysql://username:pass@localhost/dbname,mysql://'${MYSQL_USER}':'${MYSQL_PASSWORD}'@'${MYSQL_SERVER}'/'${MYSQL_DATABASE}',' deployment.ini
+    sed -i 's,file_serve_method = default,file_serve_method = nginx_redirect,' deployment.ini
+    sed -i 's,# nginx_serve_path = __mediadrop_serve__,nginx_serve_path = __mediadrop_serve__,' deployment.ini
+    sed -i 's,static_files = true,static_files = false,' deployment.ini
+    sed -i 's,enable_gzip = true,enable_gzip = false,' deployment.ini
+    sed -i 's,cache_dir = %(here)s/data,cache_dir = /wsgi/data/,' deployment.ini
+    sed -i 's,image_dir = %(here)s/data/images,image_dir = /wsgi/data/images/,' deployment.ini
+    sed -i 's,media_dir = %(here)s/data/media,media_dir = /wsgi/data/media/,' deployment.ini
+    #setup directory and permissions
+    cp -a /mediadrop/data .
+    chmod -R 777 /wsgi/data
+
+    #wsgi config
+    echo '' >> deployment.ini
+    echo '[uwsgi]' >> deployment.ini
+    echo 'socket = :9000' >> deployment.ini
+    echo 'master = true' >> deployment.ini
+    echo 'processes = 8' >> deployment.ini
+    echo 'virtualenv = /venv/mediadrop' >> deployment.ini
+    echo 'stats = 127.0.0.1:9191' >> deployment.ini
+
+    #check if database has and tables defined
+    local TESTDB="select count(*) from information_schema.tables where table_type = 'BASE TABLE' and table_schema = \"${MYSQL_DATABASE}\""
+    if [ $(mysql -ss --host=${MYSQL_SERVER} -u root -p${MYSQL_ROOT_PASSWORD} -e "${TESTDB}") == "0" ]; then
+         #setup database
+         paster setup-app deployment.ini
+         #setup advanced seach
+         mysql --host=${MYSQL_SERVER} -u root -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < /mediadrop/setup_triggers.sql
+    fi
+
+    #fix crossdomain policy
+    (cd /mediadrop/mediadrop/public && sed -i 's,\.cooliris\.com" secure="false",",' crossdomain.xml)
+}
+
+#check if volume has been initialized
+if [ ! -f /wsgi/deployment.ini ]; then
+    init_mediadrop
+fi
+
+#run uwsgi daemon
+(cd /wsgi && uwsgi --ini-paste deployment.ini)
